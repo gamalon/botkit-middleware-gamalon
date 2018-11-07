a617444e0cc0afc81d94102702ab734373cf744e
Prtfw (maddie) <shang.madeleine@gmail.com>
2018-11-06 19:48:13 -0500

    init both working

--- gamalonResponseGenerator/index.js
+++ gamalonResponseGenerator/index.js
@@ -8,22 +8,35 @@ const resetContext = () => {
   };
 };
 
+const stripSuffix = (str) => {
+  return str.substr(0, str.indexOf('_'));
+}
+
+const safePush = (arr, newInt) => {
+  arr.indexOf(newInt) === -1 ? arr.push(newInt) : console.log('already in intents')
+}
+
 const responseToClarification = (message, reply) => {
   if (reply.blocked) {
     return;
   }
 
+  console.log(16, message.text)
+  console.log(16, context.possibleResponses)
+
   if (context.waitingForClarification) {
-    if (context.possibleResponses.includes(message.text.trim().toLowerCase())) {
-      context.intents.push(message.text.trim().toLowerCase());
+    if (context.possibleResponses.map(x=>stripSuffix(x)).includes(message.text.trim().toLowerCase())) {
+      safePush(context.intents, message.text.trim().toLowerCase());
       reply.blocked = false;
+      console.log(context.intents)
       reply.steps = [{ prompt: `Thank you. So you want to ${context.intents.join(' ')}`}];
       resetContext();
     } else {
+      console.log(26, context.possibleResponses)
       reply.blocked = true;
       reply.steps = [
         { prompt: `I don't understand. Please clarify.` },
-        { prompt: `${context.possibleResponses.join(' or ')}` },
+        { prompt: `${context.possibleResponses.map(x=>stripSuffix(x)).join(' or ')}` },
       ];
     }
   }
@@ -68,13 +81,13 @@ const clarify = (message, reply) => {
         prompt: `I see that you want to ${intent}`
       });
       const step2 = ({ parentCategories }) => ({
-        prompt: `Please help us clarify: ${parentCategories.join(', ')}`,
+        prompt: `Please help us clarify: ${parentCategories.map(x=>stripSuffix(x)).join(' or ')}`,
       });
 
       reply.blocked = true;
       reply.steps = [step1, step2].map((step) => step(classification));
 
-      context.intents.push(classification.intent);
+      safePush(context.intents, classification.intent);
       context.waitingForClarification = true;
       context.possibleResponses = classification.parentCategories;
     }
@@ -113,7 +126,7 @@ const preventCancel = (message, reply) => {
   }
 
   const { gamalon } = message;
-  const allPaths = gamalon.intents.reduce((acc, { intent }) => acc.concat([intent]), []);
+  const allPaths = gamalon.subtree.intents.reduce((acc, { intent }) => acc.concat([intent]), []);
 
   if (allPaths.includes('cancel') || context.intents.includes('cancel')) {
     const cancelIndex = allPaths.findIndex((word) => word === 'cancel');
@@ -132,20 +145,63 @@ const preventCancel = (message, reply) => {
 }
 
 const executeReply = (bot, message, reply) => {
+  console.log(reply)
   reply.steps.forEach((step) => {
     bot.reply(message, step.prompt);
   });
 };
 
+
+const multi_int = (bot, message, reply) => {
+  console.log(143, 'inpath')
+  const  { gamalon } = message
+  console.log(144, gamalon.subtree.intents)
+  gamalon.subtree.intents.forEach((data, i) => {
+    console.log(147, data)
+  intnet = data.intent ? data.intent : 'UNKNOWN'
+  var resp = []
+
+
+  // console.log(path)
+  for (x of data.path){
+    // console.log(97, x)
+    if (dict[x]){
+      console.log('matched', x, dict[x])
+      pick=dict[x][Math.floor(Math.random() * dict[x].length)]
+      console.log('pick', pick)
+      resp=resp.concat(pick)
+    }
+  }
+      knownResp = resp.join('\n')// dict[intent]
+      console.log(knownResp)
+      // knownResp
+
+
+
+
+
+  bot.reply(message, knownResp)
+})
+}
+
 module.exports = (bot, message) => {
   const reply = { steps: [] };
 
-  //rules
-  responseToCancelPrevention(message, reply);
-  responseToClarification(message, reply);
-  clarify(message, reply);
-  preventCancel(message, reply);
+  // //rules
+  // responseToCancelPrevention(message, reply);
+  // responseToClarification(message, reply);
+  // clarify(message, reply);
+  // preventCancel(message, reply);
+  // executeReply(bot, message, reply);
 
+
+  multi_int(bot, message, reply)
   // reply
-  executeReply(bot, message, reply);
 };
+
+
+dict = { // note don't use empty arrays!!!
+'UNKNOWN': ["sorry I didn't catch that.", "say again?", "can you speak more slowly please."],
+'unknown': ["sorry I didn't catch that.", "say again?", "can you speak more slowly please."],
+'activation': ['activating']
+}
